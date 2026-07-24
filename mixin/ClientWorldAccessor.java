06c57f11e65b55ebf693200e7e2635d740dfc59a
feixue666LJM <153345248+feixue666LJM@users.noreply.github.com>
2026-07-24 13:51:15 +0800

    Add files via upload

---
+++ mixin/ClientWorldAccessor.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2025 Nippaku_Zanmu
+ * SPDX-License-Identifier: gplv3
+ */
+
+package com.kijinseija.seija_printer.mixin;
+
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.multiplayer.prediction.BlockStatePredictionHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ClientLevel.class)
+public interface ClientWorldAccessor {
+    @Accessor("blockStatePredictionHandler")
+    BlockStatePredictionHandler getPendingUpdateManager();
+//    @Invoker("getPendingUpdateManager")
+//    PendingUpdateManager getPendingUpdateManager();
+}
